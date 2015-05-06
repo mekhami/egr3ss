3c7a8a48bb0a670194450e5e54f2caab8fcc4c6a
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2015-05-06 10:37:47 -0400

    removed the time stamp from the output messages

--- egr3ss/utils.py
+++ egr3ss/utils.py
@@ -4,7 +4,7 @@ WARN = '\033[93m\033[1m[!] \033[0m'
 GOOD = '\033[92m\033[1m[+] \033[0m'
 INFO = '\033[94m\033[1m[*] \033[0m'
 
-FORMAT_STRING = "{0} [" + time.strftime("%x") + " - " + time.strftime("%X") + "] {1}"
+FORMAT_STRING = "{0} {1}"
 
 def info(message):
     print FORMAT_STRING.format(INFO, message)
